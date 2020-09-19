a09690b7a48a393d7580e07e69ab165e0aa56d4f
Karen Abundis <karen.abundis3673@alumnos.udg.mx>
2020-09-19 15:24:56 -0500

    Se agrego la funcion para la opcion 2

--- Test.c
+++ Test.c
@@ -1,14 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 void enteros()
 {
     int num[5],i;
     int sum=0;
 
     printf("Ingrese 5 numeros enteros:\n");
-    for( i=0;i<5;i++){
+    for(i=0;i<5;i++){
     printf(" ");
     scanf("%d",&num[i]);
     }
@@ -19,13 +18,20 @@ void enteros()
     printf("Promedio:%.2f\n",sum/5.0);
 }
 
+void mostrar(int n,char cad[]){
+    int i;
+	for(i=0;i<n;i++){
+		printf("%s",cad);
+		}
+	}
+
 int main(){
 
 	int op;
 	char cad[20];
 	int n;
 	do{
-		printf("\n1. Capturar\n2. Mostrar Cadena n Veces\n3. Agregar personaje\n4.Mostrar Personaje\n0. Salir\n");
+		printf("\n1. Capturar\n2. Mostrar Cadena n Veces\n3. Agregar personaje\n0. Salir\n");
 		scanf("%d",&op);
 
 		switch(op){
@@ -34,7 +40,12 @@ int main(){
 				break;
 			}
 			case 2:{
-
+				printf("Ingrese una cadena de hasta 20 caracteres: ");
+				fflush(stdin);
+				fgets(cad,20,stdin);
+				printf("n:");
+				scanf("%d",&n);
+				mostrar(n,cad);
 				break;
 			}
 			case 3:{
